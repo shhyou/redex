a38f1be2b5b7f41acf34159dd957c5bf80acd48e
shhyou <shu-hung.you@eecs.northwestern.edu>
2021-08-14 21:48:49 -0500

    Add more exceptions

--- redex-lib/redex/private/error.rkt
+++ redex-lib/redex/private/error.rkt
@@ -20,3 +20,30 @@
 
 (define (unimplemented pat)
   (redex-error 'generate-term "#:i-th does not yet support ~s patterns" pat))
+
+(struct exn:fail:redex:contract exn:fail:redex
+  (name contract terms))
+
+(struct exn:fail:redex:contract-judgment exn:fail:redex:contract (judgment))
+(struct exn:fail:redex:contract-relation exn:fail:redex:contract (relation))
+
+(struct exn:fail:redex:metafunction exn:fail:redex
+  (name))
+
+(struct exn:fail:redex:no-pattern-matched exn:fail:redex:metafunction
+  ())
+
+(struct exn:fail:redex:pattern-multiple-matches exn:fail:redex:metafunction
+  ())
+
+(struct exn:fail:redex:match-different-results exn:fail:redex:metafunction
+  ())
+
+(struct exn:fail:redex:not-in-domain exn:fail:redex:metafunction
+  ())
+
+(struct exn:fail:redex:codomain-test-failed exn:fail:redex:metafunction
+  ())
+
+(struct exn:fail:redex:no-clauses-matched exn:fail:redex:metafunction
+  ())
